136d1dea20e3b12cb12b77cca6e5529955c450c1
Rich Baukovic <rich.baukovic@brickfish.com>
2017-03-14 23:45:13 -0500

    views for car and cars now displaying -order mattered here

--- js/main.js
+++ js/main.js
@@ -2,18 +2,11 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+// Car singular
 var Car = Backbone.Model.extend();
 
-var Vehicles = Backbone.Collection.extend({
-  model: Car
-});
-
 var CarView = Backbone.View.extend({
-  tagName: "li",
-  className: "vehicle",
-  attributes: {
-    "data-color": ""
-  },
 
   render: function(){
     this.$el.html(this.model.get("registrationNumber"));
@@ -22,19 +15,32 @@ var CarView = Backbone.View.extend({
   }
 });
 
+var car = new Car({ registrationNumber: "SLA987" });
+
+var carView = new CarView({ el: "#container", model: car });
+// carView.render();
+
+// Cars plural
+var Vehicles = Backbone.Collection.extend({ 
+	model: Car,
+ 
+});
+
 var CarsView = Backbone.View.extend({
   render: function(){
+    var self = this;
+
     this.model.each(function(car){
       var carView = new CarView({ model: car });
-      this.$el.append(carView.render().$el);
+      self.$el.append(carView.render().$el);
     });
   }
 });
 
 var vehicles = new Vehicles([
-  new Car({ registrationNumber: "XLI887" }),
-  new Car({ registrationNumber: "ZNP123" }),
-  new Car({ registrationNumber: "XUV456" })
+	new Car({ registrationNumber: "XLI887", color: "Blue" }),
+	new Car({ registrationNumber: "ZNP123", color: "Blue" }),
+	new Car({ registrationNumber: "XUV456", color: "Gray" })
 ]);
 
 var carsView = new CarsView({ el: "#cars", model: vehicles });
